7414798dc26469ea21205773f2b0514d592815dd
gutack54321 <gutack1488@gmail.com>
2023-07-02 10:30:14 +0900

    add: 기본 스웨거 작성

---
+++ server/server-application/src/main/java/com/saecdo18/serverapplication/swagger/SwaggerConfig.java
@@ -0,0 +1,39 @@
+package com.saecdo18.serverapplication.swagger;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+
+@Configuration
+@EnableWebMvc
+public class SwaggerConfig {
+    @Bean
+    public InternalResourceViewResolver defaultViewResolver() {
+        return new InternalResourceViewResolver();
+    }
+    @Bean
+    public Docket swaggerApi() {
+        return new Docket(DocumentationType.OAS_30)
+                .useDefaultResponseMessages(false)
+                .select()
+//                .apis(RequestHandlerSelectors.basePackage("com.saecdo18.serverapplication.member.controller"))
+                .apis(RequestHandlerSelectors.any())
+                .paths(PathSelectors.any())
+                .build()
+                .apiInfo(mySwaggerInfo());
+    }
+
+    private ApiInfo mySwaggerInfo() {
+        return new ApiInfoBuilder()
+                .title("SwaggerTest API")
+                .description("SwaggerTest API Docs")
+                .build();
+    }
+}
