5aba4a00883a9419004cf4493920472e79ce09a3
JUNSEOK <kd02109@gmail.com>
2023-07-03 20:47:24 +0900

    Dep: 라이브러리 설치
    - 기타 모든 라이브러리 설치

--- client/.eslintrc.cjs
+++ client/.eslintrc.cjs
@@ -1,39 +1,38 @@
 module.exports = {
-  env: { browser: true, es2020: true, node: true },
-  extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:react-hooks/recommended',
-    'plugin:prettier/recommended',
-    'plugin:import/typescript',
-    'plugin:import/recommended',
-  ],
+  env: {
+    browser: true,
+    es2020: true,
+    node: true
+  },
+  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'plugin:react-hooks/recommended', 'plugin:prettier/recommended', 'plugin:import/typescript', 'plugin:import/recommended', 'plugin:storybook/recommended'],
   parser: '@typescript-eslint/parser',
-  parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module'
+  },
   plugins: ['react-refresh', 'prettier', 'import'],
   settings: {
     'import/resolver': {
       node: {},
       typescript: {
-        directory: './src',
-      },
+        directory: './src'
+      }
     },
-    'import/parsers': { '@typescript-eslint/parser': ['.ts', '.tsx'] },
+    'import/parsers': {
+      '@typescript-eslint/parser': ['.ts', '.tsx']
+    }
   },
   rules: {
     'react-refresh/only-export-components': 'warn',
-    'import/order': [
-      'error',
-      {
-        groups: ['builtin', 'external', 'parent', 'sibling', 'index'],
-        alphabetize: {
-          order: 'asc',
-          caseInsensitive: true,
-        },
-        'newlines-between': 'never',
+    'import/order': ['error', {
+      groups: ['builtin', 'external', 'parent', 'sibling', 'index'],
+      alphabetize: {
+        order: 'asc',
+        caseInsensitive: true
       },
-    ],
+      'newlines-between': 'never'
+    }],
     'import/no-unresolved': 'off',
-    'import/export': 'off',
-  },
+    'import/export': 'off'
+  }
 };
