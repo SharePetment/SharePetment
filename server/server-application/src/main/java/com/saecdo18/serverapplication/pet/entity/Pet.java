8e867cacf342a4be56215535d3a51aec7c13a6c9
gutack54321 <gutack1488@gmail.com>
2023-07-03 11:36:54 +0900

    add: 회원, 반려동물, 팔로우 엔티티 추가

---
+++ server/server-application/src/main/java/com/saecdo18/serverapplication/pet/entity/Pet.java
@@ -0,0 +1,38 @@
+package com.saecdo18.serverapplication.pet.entity;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.saecdo18.serverapplication.member.entity.Member;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Getter
+@Setter
+@Entity
+@NoArgsConstructor
+public class Pet {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long petId;
+    @Column(nullable = false)
+    private String profile;
+    @Column(nullable = false)
+    private String name;
+    @Column(nullable = false)
+    private int age;
+    @Column(nullable = false)
+    private String sex;
+    @Column(nullable = false)
+    private String species;
+    private String information;
+    private String statusMessage;
+    @Column(nullable = false)
+    private boolean isWalkMated=false;
+
+    @JsonIgnore
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "MEMBER_ID")
+    private Member member;
+}
