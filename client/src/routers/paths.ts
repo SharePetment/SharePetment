e41109852da411b865785a0f97a3d38e7a425704
ddaeunbb <ddaeunbb@gmail.com>
2023-07-04 10:19:09 +0900

    Feat: 웹페이지 라우팅 경로 설정
    - 웹 페이지 라우팅 경로를 routers 폴더에 enum으로 설정해두었습니다.

---
+++ client/src/routers/paths.ts
@@ -0,0 +1,14 @@
+export enum Direction {
+  Login = '/',
+  SignUp = '/sign-up',
+  Home = '/home',
+  Mypage = '/my-page',
+  Userpage = '/users/:usersId',
+  FeedPopUp = '/home/:feedId',
+  FeedPosting = '/feed-posting',
+  FeedEditing = '/feed-posting/:feedId',
+  WalkMate = '/walkmate',
+  WalkFeed = 'walkmate/:postId',
+  WalkPosting = '/walk-posting',
+  WalkEditing = '/walk-posting/:postId',
+}
