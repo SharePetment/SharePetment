f866f9645097afe827dd6510c991b3cf07015739
ddaeunbb <ddaeunbb@gmail.com>
2023-07-04 23:05:17 +0900

    Dep: tailwind.config.js 파일 수정
    - 색상을 커스텀하여 사용하기 위해 tailwind.config 파일을 수정하였습니다.
    - App.tsx 파일에서 커스텀 색상이 적용되는지 테스트해보았습니다.

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,13 @@
+import tw from 'tailwind-styled-components';
+
+const Button = tw.button`
+  w-9
+  h-9
+  bg-[lightgray]
+`;
+
 function App() {
-  return <div>App</div>;
+  return <Button>안녕하세용</Button>;
 }
 
 export default App;
--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -2,7 +2,14 @@
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    colors: {
+      bg: '#fafafa',
+      text: '#171717',
+      deepgray: '#a1a1aa',
+      lightgray: 'd4d4d8',
+      green: '#69B783',
+      lightgreen: '#CCE7C2',
+    },
   },
   plugins: [],
 };
