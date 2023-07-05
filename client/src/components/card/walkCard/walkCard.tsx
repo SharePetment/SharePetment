53cdf20b758fa863fc129589c79e38a311dee105
leejaelll <jayaom91076@gmail.com>
2023-07-05 23:41:24 +0900

    Feat: walkCard Component 생성
    - walkCard 컴포넌트는 time,title,freinds,location,isclosed를 props로 받는다.
    - isclosed가 true일 경우, 모집완료를 띄워준다.

---
+++ client/src/components/card/walkCard/walkCard.tsx
@@ -0,0 +1,69 @@
+import { ReactComponent as Dog } from '../../../assets/dog.svg';
+import { ReactComponent as Paw } from '../../../assets/paw.svg';
+import { ReactComponent as Pin } from '../../../assets/pin.svg';
+import Profile from '../../../common/profile/Profile';
+import { BooleanStr } from '../../../types/buttonType';
+import {
+  Backdrop,
+  CardContainer,
+  CompoleteNotice,
+  WalkDate,
+  Title,
+  WalkMateList,
+  WalkMateItem,
+  LocationTitle,
+  WriterProfile,
+  CompoleteText,
+} from './walkCard.styled';
+
+type walkCardProps = {
+  time: string;
+  title: string;
+  freinds: number;
+  location: string;
+  chaturl: string;
+  isclosed: BooleanStr;
+};
+
+export default function WalkCard({
+  time,
+  title,
+  freinds,
+  location,
+  isclosed,
+}: walkCardProps) {
+  return (
+    <CardContainer>
+      <WalkDate>
+        <div className="text-xs">산책날짜</div>
+        <div>{time}</div>
+      </WalkDate>
+      <Title>{title}</Title>
+      <WalkMateList>
+        <WalkMateItem>
+          <Dog className="shrink-0" />
+          <span>{freinds}마리</span>
+        </WalkMateItem>
+        <WalkMateItem>
+          <Pin className="shrink-0" />
+          <LocationTitle>{location}</LocationTitle>
+        </WalkMateItem>
+      </WalkMateList>
+      <WriterProfile>
+        {/* TODO:이후 수정 필요 */}
+        <Profile size="sm" url="https://huchu.link/MZFVNjh" isgreen="false" />
+        <span className="text-semibold">Hello_world</span>
+      </WriterProfile>
+      {isclosed === 'false' && (
+        <>
+          <Backdrop>
+            <CompoleteNotice className="flex items-center">
+              <Paw className="shrink-0" />
+              <CompoleteText>모집 완료</CompoleteText>
+            </CompoleteNotice>
+          </Backdrop>
+        </>
+      )}
+    </CardContainer>
+  );
+}
