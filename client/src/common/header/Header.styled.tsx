68376dd35f5f85c84034c781741d97e2af354d73
leejaelll <jayaom91076@gmail.com>
2023-07-05 23:43:50 +0900

    Style: Header Component 스타일 수정 - padding 추

--- client/src/common/header/Header.styled.tsx
+++ client/src/common/header/Header.styled.tsx
@@ -1,17 +1,20 @@
 import tw from 'tailwind-styled-components';
-
 export const HeaderContainer = tw.div`
   w-full
   flex
   justify-between
   items-center
+  px-6
+  py-4
 `;
 
 export const NavList = tw.ul`
-flex gap-5
-items-center
+  flex gap-5
+  items-center
+`;
 
-li {
-  text-9xl	
-}
+export const NavItem = tw.li<{ currenttab: number; idx: number }>`
+  font-bold
+  cursor-pointer
+  ${prop => (prop.currenttab === prop.idx ? `text-deepgreen` : `text-deepgray`)}
 `;
