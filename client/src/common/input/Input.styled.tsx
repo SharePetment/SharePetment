f85997106f46605809934991d9b15c65b18c40bf
leejaelll <jayaom91076@gmail.com>
2023-07-07 02:55:21 +0900

    Refactor: Input Component 공통컴포넌트 제거
    - Input 컴포넌트를 공통으로 관리하는 것이 어려워 common에서 제거
    - label과 input 스타일만 공통으로 관리

--- client/src/common/input/Input.styled.tsx
+++ client/src/common/input/Input.styled.tsx
@@ -1,32 +1,39 @@
+import { FieldError, FieldErrorsImpl, Merge } from 'react-hook-form';
 import tw from 'tailwind-styled-components';
-import { BooleanStr } from '../../types/propType';
+
+type ErrorMessage = {
+  error?:
+    | string
+    | FieldError
+    | Merge<FieldError, FieldErrorsImpl<any>>
+    | undefined;
+};
 
 export const InputContainer = tw.div`
   w-80
   relative
 `;
 
-export const InputLabel = tw.label`
-  absolute
-  top-0
+export const Label = tw.label`
   block
-  -top-6
-  font-bold
+  font-extrabold
+  text-defaulttext
 `;
 
-export const InputText = tw.input<{ isError: BooleanStr }>`
+export const InputText = tw.input<ErrorMessage>`
   w-full
   py-3.5
   px-5
   border
   rounded-[10px]
-  ${prop => prop.isError === 'true' && `border-[#FF7B7B]`}
+
+  ${prop => prop.error === undefined && `border-deepgray`}
+  ${prop => typeof prop.error === 'string' && `border-[#FF7B7B]`}
 `;
 
 export const ErrorNotice = tw.p`
   text-[#FF7B7B]
   absolute
-  -top-4
+  top-0
   right-0
-  text-xs
 `;
