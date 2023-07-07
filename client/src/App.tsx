8c596330266093ea5e02b0beedc081ec5c209510
JUNSEOK <kd02109@gmail.com>
2023-07-07 10:08:58 +0900

    Feat: Header 분기 처리
    What is this PR?
    - `/` | `/info` 인경우 Header가 보이지 않도록 설정했습니다.
    -  walkCard 라이브러리 경로 재설정

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,36 +1,16 @@
-import { useState } from 'react';
-import { useNavigate, Outlet } from 'react-router-dom';
+import { Outlet, useMatch } from 'react-router-dom';
 import Footer from './common/footer/Footer';
 import Header from './common/header/Header';
-import Popup from './common/popup/Popup';
 
 function App() {
-  const navigate = useNavigate();
-  const [isOpened, setIsOpened] = useState(false);
-  const handleClose = () => {
-    setIsOpened(false);
-  };
-  const handleNavigate = () => {
-    setIsOpened(false);
-    navigate('/123');
-  };
+  const matchHome = useMatch('/'); //object || null
+  const matchInfo = useMatch('/info'); //object || null
 
   return (
     <>
-      <Header isloginuser="true" />
+      {!(matchHome || matchInfo) && <Header isloginuser="true" />}
       <Outlet />
       <Footer />
-      {isOpened && (
-        <Popup
-          title="회원가입을 하셔야 사용합니다"
-          handler={[handleClose, handleNavigate]}
-          btnsize={['md', 'md']}
-          countbtn={2}
-          buttontext={['닫기입니다.', '확인입니다.']}
-          isgreen={['true', 'false']}
-          popupcontrol={handleClose}
-        />
-      )}
     </>
   );
 }
