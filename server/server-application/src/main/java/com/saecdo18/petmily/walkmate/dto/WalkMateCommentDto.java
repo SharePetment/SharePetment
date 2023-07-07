32c6efbd3a830d7e69187e038b96841fc24382aa
gibae <petergbson2@naver.com>
2023-07-07 15:48:56 +0900

    fix: 브랜치 제거 후 덮어쓰기

---
+++ server/server-application/src/main/java/com/saecdo18/petmily/walkmate/dto/WalkMateCommentDto.java
@@ -0,0 +1,44 @@
+package com.saecdo18.petmily.walkmate.dto;
+
+import com.saecdo18.petmily.member.dto.MemberDto;
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Data
+public class WalkMateCommentDto {
+
+    //    private String content;
+//    private Integer likes;
+//    private WalkMate walkMate;
+//    private Member member;
+    @Getter
+    @Setter
+    public static class Post{
+
+        private String content;
+    }
+
+    @Getter
+    @Setter
+    public static class Patch{
+
+        private String content;
+    }
+
+    @Getter
+    @Setter
+    public static class Response{
+
+        private Long commentId;
+        private Long walkMatePostId;
+        private Long memberId;
+        private MemberDto.Info memberInfo;
+        private String content;
+        private Integer likes;
+        private LocalDateTime createdAt;
+        private LocalDateTime modifiedAt;
+    }
+}
