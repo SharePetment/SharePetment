f42e8a7a2359d784870823e60bc95d57877c5eaf
ddaeunbb <ddaeunbb@gmail.com>
2023-07-10 17:26:37 +0900

    Fix: 새로고침 시, 헤더의 값이 변경되는 버그 해결
    - 새로고침 시, 헤더값이 변경되는 버그를 useMatch를 활용해 해결해였습니다.

--- client/src/common/header/Header.tsx
+++ client/src/common/header/Header.tsx
@@ -1,27 +1,17 @@
-import { useState } from 'react';
-import { Link, useNavigate } from 'react-router-dom';
+import { useMatch, Link, useNavigate } from 'react-router-dom';
 import { ReactComponent as Logo } from '../../assets/logo.svg';
-import User from '../../assets/user.png';
 import Path from '../../routers/paths';
 import Button from '../button/Button';
 import Profile from '../profile/Profile';
 import { HeaderContainer, NavItem, NavList } from './Header.styled';
 
 export default function Header() {
+  const matchHome = useMatch('/home');
+  const matchWalkmate = useMatch('/walkmate');
+  const matchPost = useMatch('/feed-posting');
   const memberId = localStorage.getItem('memberId');
   const animalParents = localStorage.getItem('animalParents');
-  console.log(typeof memberId);
   const navigate = useNavigate();
-  const [currentTab, setCurrentTab] = useState(0);
-  const selectTabHandler = (index: number) => {
-    setCurrentTab(index);
-  };
-
-  const tabs = [
-    { name: '홈', link: Path.Home },
-    { name: '산책', link: Path.WalkMate },
-    { name: '포스트', link: Path.FeedPosting },
-  ];
 
   return (
     <HeaderContainer>
@@ -33,40 +23,31 @@ export default function Header() {
       <nav>
         <NavList>
           {memberId === null && (
-            <NavItem
-              onClick={() => selectTabHandler(0)}
-              currenttab={currentTab}
-              idx={0}>
+            <NavItem active={matchHome !== null}>
               <Link to={Path.Home}>홈</Link>
             </NavItem>
           )}
           {memberId !== null && animalParents === 'false' ? (
             <>
-              <NavItem
-                onClick={() => selectTabHandler(0)}
-                currenttab={currentTab}
-                idx={0}>
+              <NavItem active={matchHome !== null}>
                 <Link to={Path.Home}>홈</Link>
               </NavItem>
-              <NavItem
-                onClick={() => selectTabHandler(1)}
-                currenttab={currentTab}
-                idx={1}>
+              <NavItem active={matchPost !== null}>
                 <Link to={Path.FeedPosting}>포스트</Link>
               </NavItem>
             </>
           ) : (
-            tabs.map((tab, index) => {
-              return (
-                <NavItem
-                  key={index}
-                  onClick={() => selectTabHandler(index)}
-                  currenttab={currentTab}
-                  idx={index}>
-                  <Link to={tab.link}>{tab.name}</Link>
-                </NavItem>
-              );
-            })
+            <>
+              <NavItem active={matchHome !== null}>
+                <Link to={Path.Home}>홈</Link>
+              </NavItem>
+              <NavItem active={matchWalkmate !== null}>
+                <Link to={Path.WalkMate}>산책</Link>
+              </NavItem>
+              <NavItem active={matchPost !== null}>
+                <Link to={Path.FeedPosting}>포스트</Link>
+              </NavItem>
+            </>
           )}
 
           <li>
