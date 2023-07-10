9e45061abc5c379fce18a2f835ab46e03541ad82
leejaelll <jayaom91076@gmail.com>
2023-07-10 13:40:04 +0900

    Feat: x버튼 클릭 시, useNavigate 사용하여 이전페이지로 이동

--- client/src/common/popup/popup.styled.tsx
+++ client/src/common/popup/popup.styled.tsx
@@ -16,11 +16,10 @@ export const PopupBackGround = tw.div`
     w-full
     h-full
     flex
-    justify-center 
+    justify-center
     items-center
-    z-200
-    bg-zinc-900
-    opacity-80
+    z-[9999]
+    bg-zinc-900/75
 `;
 
 export const PopupBox = tw.div`
@@ -36,6 +35,10 @@ export const PopupBox = tw.div`
     max-sm:w-3/4
     w-1/3
     border
+    absolute
+    top-[50%]
+    left-[50%]
+    transform -translate-x-1/2 -translate-y-1/2
 `;
 
 export const Title = tw.h1`
