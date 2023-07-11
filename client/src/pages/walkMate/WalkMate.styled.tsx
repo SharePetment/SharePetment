921245b0809740ae8dc7acb59a8706358ab0346a
leejaelll <jayaom91076@gmail.com>
2023-07-11 20:15:00 +0900

    Feat: WalkMate 컴포넌트 생성

---
+++ client/src/pages/walkMate/WalkMate.styled.tsx
@@ -0,0 +1,12 @@
+import tw from 'tailwind-styled-components';
+
+export const SelectContainer = tw.div`
+flex
+`;
+
+export const SearchButton = tw.button`
+  h-[50px]
+  text-base
+  rounded-2xl
+  bg-deepgreen
+`;
--- client/src/pages/walkMate/WalkMate.tsx
+++ client/src/pages/walkMate/WalkMate.tsx
@@ -1,27 +1,55 @@
-// import { useQuery } from '@tanstack/react-query';
+import { useInfiniteQuery } from '@tanstack/react-query';
 import { useState } from 'react';
+// import { useInView } from 'react-intersection-observer';
 import { Link } from 'react-router-dom';
-// import { getWalkmateList } from '../../api/queryfn';
+import { useReadLocalStorage } from 'usehooks-ts';
+import { getServerDataWithJWT } from '../../api/queryfn';
+import { SERVER_URL } from '../../api/url';
 import { ReactComponent as Plus } from '../../assets/button/plus.svg';
 import { GridContainer } from '../../common/grid/Grid.styled';
 import Select from '../../common/select/Select';
 import WalkCard from '../../components/card/walkCard/walkCard';
 import { CardContainer } from '../../components/card/walkCard/walkCard.styled';
 import Path from '../../routers/paths';
+import { WalkFeedList } from '../../types/walkType';
+import { SearchButton, SelectContainer } from './WalkMate.styled';
 
 export function Component() {
   // 주소 값 받아오기
-  const [zip, setZip] = useState('');
-  console.log(zip);
+  const [zip, setZip] = useState('서울특별시');
 
-  // const { data: walkMateData } = useQuery({
-  //   queryKey: ['walkmateList'],
-  //   queryFn: () => getWalkmateList(),
-  // });
+  const accessToken = useReadLocalStorage<string | null>('accessToken');
+
+  /* ---------------------------- useInfiniteQuery ---------------------------- */
+  const { data, refetch, isFetching, isError, hasNextPage, fetchNextPage } =
+    useInfiniteQuery<WalkFeedList>({
+      queryKey: ['walkmateList'],
+      queryFn: ({ pageParam = 0 }) =>
+        getServerDataWithJWT(
+          `${SERVER_URL}walkmates/walks?openFilter=false&location=${zip}&page=${pageParam}&size=10`,
+          accessToken,
+        ),
+      getNextPageParam: (lastPage, allPages) => {
+        console.log(lastPage, allPages);
+        // const nextPage = allPages.length + 1;
+        // return lastPage.currentPage < lastPage.totalPage ? nextPage : undefined;
+      },
+    });
+
+  const handleClickSearchAddress = () => {
+    refetch();
+  };
 
   return (
     <div className="mx-40 py-10 max-md:mx-20 ">
-      <Select size="md" direction="row" setZip={setZip} />
+      <SelectContainer className="justify-end">
+        <Select size="md" direction="row" setZip={setZip} />
+        <SearchButton
+          onClick={handleClickSearchAddress}
+          className=" flex-shrink-0 w-20 text-defaultbg">
+          검색
+        </SearchButton>
+      </SelectContainer>
       <GridContainer>
         <Link to={Path.WalkPosting}>
           <CardContainer className="items-center justify-center">
