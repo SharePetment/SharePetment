3a7e7d24889430ec58ee18eb9e7ff0433a6363d8
leejaelll <jayaom91076@gmail.com>
2023-07-11 20:15:46 +0900

    Feat: WalkMate 타입 파일 추가

---
+++ client/src/types/walkType.ts
@@ -0,0 +1,23 @@
+import { MemberInfo } from './commentType';
+
+// 산책 게시물 리스트
+export interface WalkFeedList {
+  walkMatePostId: number;
+  memberId: number;
+  title: string;
+  content: string;
+  mapURL: string;
+  chatURL: string;
+  location: string;
+  time: string;
+  open: boolean;
+  maximum: string;
+  likeCount: number;
+  createdAt: number[];
+  modifiedAt: number[];
+}
+
+// 산책 게시물 디테일
+export interface WalkFeed extends WalkFeedList {
+  memberInfo: MemberInfo;
+}
