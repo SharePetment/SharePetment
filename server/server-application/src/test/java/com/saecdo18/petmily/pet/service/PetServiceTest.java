b05d5cc3ef55387a56e4b91ee43355edddc22fd3
gutack54321 <gutack1488@gmail.com>
2023-07-14 13:25:52 +0900

    feat: 펫 서비스 테스트

---
+++ server/server-application/src/test/java/com/saecdo18/petmily/pet/service/PetServiceTest.java
@@ -0,0 +1,31 @@
+package com.saecdo18.petmily.pet.service;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@ExtendWith(MockitoExtension.class)
+class PetServiceTest {
+
+    @Test
+    void createPet() {
+    }
+
+    @Test
+    void getPet() {
+    }
+
+    @Test
+    void updatePet() {
+    }
+
+    @Test
+    void deletePet() {
+    }
+
+    @Test
+    void savePetImage() {
+    }
+}
