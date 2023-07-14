54d758f3f4a5b386200d0bfaa7b31c1dad7abb64
leejaelll <jayaom91076@gmail.com>
2023-07-14 14:03:34 +0900

    Refactor: url 변경

--- client/src/components/notice/NoticeOnlyOwner.tsx
+++ client/src/components/notice/NoticeOnlyOwner.tsx
@@ -9,7 +9,9 @@ export default function NoticeOnlyOwner() {
       <ErrorText>견주만 접근가능합니다.</ErrorText>
       <ErrorText>반려동물 등록하러 갈까요?</ErrorText>
       <img src={LoginPets} className=" w-80" />
-      <HomeBtn onClick={() => navigate('/')}>반려동물 등록하러가기</HomeBtn>
+      <HomeBtn onClick={() => navigate('/my-page')}>
+        반려동물 등록하러가기
+      </HomeBtn>
     </div>
   );
 }
