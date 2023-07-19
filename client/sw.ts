72ea630dbef3c9c97b2c95d499b35071b87cafd5
JUNSEOK <kd02109@gmail.com>
2023-07-19 16:34:03 +0900

    Fix: pwa 수정
    What is this PR?
    - service  Worker 직접 구현
    - vite-plugin-pwa package 제거

---
+++ client/sw.ts
@@ -0,0 +1,14 @@
+// install event
+self.addEventListener('install', () => {
+  console.log('[Service Worker] installed');
+});
+
+// activate event
+self.addEventListener('activate', e => {
+  console.log('[Service Worker] actived', e);
+});
+
+// fetch event
+self.addEventListener('fetch', (e: any) => {
+  console.log('[Service Worker] fetched resource ' + e.request.url);
+});
--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,44 +1,9 @@
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
-import { VitePWA, VitePWAOptions } from 'vite-plugin-pwa';
 import svgr from 'vite-plugin-svgr';
-
-const manifestForPlugin: Partial<VitePWAOptions> = {
-  registerType: 'prompt',
-  includeAssets: ['favicon.svg', 'apple-touch-icon.png'],
-  manifest: {
-    name: 'Share Petment',
-    short_name: 'Petment',
-    description: 'Share Your Pet and Make Pet friends',
-    icons: [
-      {
-        src: '/android-chrome-192x192.png',
-        sizes: '192x192',
-        type: 'image/png',
-      },
-      {
-        src: '/android-chrome-512x512.png',
-        sizes: '512x512',
-        type: 'image/png',
-      },
-      {
-        src: '/apple-touch-icon.png',
-        sizes: '180x180',
-        type: 'image/png',
-        purpose: 'apple touch icon',
-      },
-    ],
-    theme_color: '#171717',
-    background_color: '#e8ebf2',
-    display: 'standalone',
-    scope: '/',
-    start_url: './index.html',
-    orientation: 'portrait',
-  },
-};
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: './',
-  plugins: [react(), svgr(), VitePWA(manifestForPlugin)],
+  plugins: [react(), svgr()],
 });
