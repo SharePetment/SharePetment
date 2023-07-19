90b63853ce7d99535ed5d00e417dbf06d3fede92
leejaelll <jayaom91076@gmail.com>
2023-07-19 10:54:28 +0900

    Style: NavBar 컴포넌트 CSS 수정

--- client/src/components/nav/NavBar.tsx
+++ client/src/components/nav/NavBar.tsx
@@ -31,7 +31,7 @@ export default function NavBar() {
   if (accessToken) {
     return (
       <nav className="hidden max-sm:block bg-white fixed bottom-[16px] rounded-[30px] left-1/2 -translate-x-[50%] z-[800] shadow-xl">
-        <ul className="flex gap-8 py-3 px-10">
+        <ul className="flex gap-8 py-3 px-10 max-sm:px-7 max-sm:gap-7">
           <Link to={Path.Home}>
             <li>
               {matchHome !== null ? (
