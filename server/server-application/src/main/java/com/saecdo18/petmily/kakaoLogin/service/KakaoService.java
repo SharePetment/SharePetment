8afc7029d0d11898874bd3ede8c5f09689836e4c
junyoungs7 <junyoungs7@naver.com>
2023-07-24 10:45:48 +0900

    Fix: redis expire time & guest feed

--- server/server-application/src/main/java/com/saecdo18/petmily/kakaoLogin/service/KakaoService.java
+++ server/server-application/src/main/java/com/saecdo18/petmily/kakaoLogin/service/KakaoService.java
@@ -176,12 +176,12 @@ public class KakaoService {
         queryParams.add("animalParents", String.valueOf(member.isAnimalParents()));
 
         return UriComponentsBuilder.newInstance()
-                .scheme("http")
+                .scheme("https")
 //                .host("localhost")
 //                .host("share-petment.s3-website.ap-northeast-2.amazonaws.com")
 //                .host("share-petment.netlify.app")
 //                .port(5374)
-                .host("https://sharepetment.site/")
+                .host("sharepetment.site")
                 .path("loading")
                 .queryParams(queryParams)
                 .build()
